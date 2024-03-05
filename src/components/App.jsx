97f3deff021572a6cd3b51c8fcea5bbb6017855b
Olena-Ishchenko <elena.dubovalova@gmail.com>
2024-03-05 17:57:28 +0200

    profile

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+
+import userData from '../userData.json';
+import Profile from './Profile/Profile';
+import '../App.css';
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+
+export default App;
